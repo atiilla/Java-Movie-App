7d2bb4eaf7af1b4484fe6e709802ebb0b51e38be
atiilla <9992685+atiilla@users.noreply.github.com>
2025-01-06 13:58:37 +0100

    * Maven project created * Deps installed * README created for basic info about software

---
+++ netflix/src/main/java/com/netflix/App.java
@@ -0,0 +1,16 @@
+package com.netflix;
+/*
+mvn compile && mvn exec:java -Dexec.mainClass="com.netflix.App"
+mvn clean package && java -jar target/netflix-1.0-SNAPSHOT.jar
+ */
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
