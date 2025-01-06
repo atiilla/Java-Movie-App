f82b688e91b5d7ae79db7f8ea7c3ed50116601b1
atiilla <9992685+atiilla@users.noreply.github.com>
2025-01-06 14:50:40 +0100

    csv reading tests

--- netflix/src/main/java/com/netflix/App.java
+++ netflix/src/main/java/com/netflix/App.java
@@ -1,6 +1,15 @@
 package com.netflix;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
 import picocli.CommandLine;
+
+
 
 /*
 mvn compile && mvn exec:java -Dexec.mainClass="com.netflix.App"
@@ -12,9 +21,27 @@ mvn clean package && java -jar target/netflix-1.0-SNAPSHOT.jar
  */
 public class App 
 {
+    private static final String FILE_PATH = "src/main/resources/netflix_titles.csv";
+
     public static void main( String[] args )
     {
-        // System.out.println( "Hello World!" );
+        // show_id,type,title,director,cast,country,date_added,release_year,rating,duration,listed_in,description
+
+        List<String[]> movies = readCsv();
+        if (movies == null) {
+            System.out.println("Error reading CSV file");
+            return;
+        }
+
+        if(movies != null) {
+            // for (String[] movie : movies) {
+                
+            // }
+            System.out.println("Total movies: " + movies.size()); // movies count
+        }
+
+
+
 /*
  ─▄▀─▄▀
 ──▀──▀
@@ -32,6 +59,18 @@ public class App
         );
 
         CommandLine commandLine = new CommandLine(new CLI());
-        commandLine.execute(args);
+        // commandLine.execute(args);
     }
+
+
+    public static List<String[]> readCsv() {
+        try (CSVReader reader = new CSVReader(new FileReader(FILE_PATH))) {
+            return reader.readAll();
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    
 }
